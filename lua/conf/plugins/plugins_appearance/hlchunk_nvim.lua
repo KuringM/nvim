6f4b3710e9ec34951add3cb638698267fc3623cf
Kuring <kuring@MdeMacBook-Air.local>
2023-10-27 23:01:03 +0800

    update

--- lua/conf/plugins/plugins_appearance/hlchunk_nvim.lua
+++ lua/conf/plugins/plugins_appearance/hlchunk_nvim.lua
@@ -1,34 +1,26 @@
-return {
-	{ -- This is the lua implementation of nvim-hlchunk, you can use this neovim plugin to highlight your indent line and the current chunk (context) your cursor stayed
-		"shellRaining/hlchunk.nvim",
-		init = function()
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
-			require('hlchunk').setup({
-				chunk = {
-					enable = true,
-					use_treesitter = false,
-					style = {
-						{ fg = "#806d9c" },
-					},
+return 
+{ -- This is the lua implementation of nvim-hlchunk, you can use this neovim plugin to highlight your indent line and the current chunk (context) your cursor stayed
+	"shellRaining/hlchunk.nvim",
+	init = function()
+		vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
+		require('hlchunk').setup({
+			chunk = {
+				enable = true,
+				use_treesitter = false,
+				style = {
+					{ fg = "#806d9c" },
 				},
-				indent = {
-					chars = { "│", "¦", "┆", "┊", },
-					use_treesitter = false,
-				},
-				blank = {
-					enable = false,
-				},
-				line_num = {
-					use_treesitter = true,
-				},
-			})
-		end
-	},
-	-- {
-	-- 	"lukas-reineke/indent-blankline.nvim",
-	-- 	opts = {
-	-- 		show_current_context = true,
-	-- 		show_current_context_start = false,
-	-- 	}
-	-- },
+			},
+			indent = {
+				chars = { "│", "¦", "┆", "┊", },
+				use_treesitter = false,
+			},
+			blank = {
+				enable = false,
+			},
+			line_num = {
+				use_treesitter = true,
+			},
+		})
+	end
 }
