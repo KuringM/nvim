69241e18eb90142b4b554d1ac963365bb3aec6c2
Kuring <kuring@MdeMacBook-Air.local>
2023-11-06 21:34:41 +0800

    update Markdown plugin

--- lua/_machine_specific_default.lua
+++ lua/_machine_specific_default.lua
@@ -1,26 +1,26 @@
+-- Snippets
 -- please read through this file and change what you need to change
 vim.g.snips_author = 'Kuring'
 
 -- Python
-vim.g.python_host_prog='/usr/bin/python2'
-vim.g.python3_host_prog='/usr/bin/python3'
+-- vim.g.python_host_prog = '/usr/bin/python2'
+vim.g.python3_host_prog = '/usr/bin/python3'
 
 -- Ruby
 vim.g.ruby_host_prog = '/usr/bin/ruby'
---[[
--- Flutter
-vim.g.flutter_default_device = 'iPhone'
-vim.g.flutter_run_args = ----flavor dev--
 
+-- Flutter
+--vim.g.flutter_default_device = 'iPhone'
+--vim.g.flutter_run_args = ----flavor dev--
+
+-- Markdown
 -- App variable 
 -- vim.g.mkdp_browser = 'google-chrome-stable'
-
--- command open Google Chrome on mac
-Open_browser = function(url)
-    vim.cmd([[silent exe 'silent !open -a --Google Chrome-- ' . a:url]])
-end
-vim.g.mkdp_browserfunc = Open_browser
-
+--
+-- -- command open Google Chrome on mac
+-- Open_browser = function(url)
+--   vim.cmd([[silent exe 'silent !open -a --Google Chrome-- ' . a:url]])
+-- end
+-- vim.g.mkdp_browserfunc = Open_browser
 -- vim.g.mkdp_browser = 'chromium'
 -- vim.g.mkdp_browserfunc = 'open '
---]]
--- lua/conf/plugins/plugins_lsp/markdown.lua
+++ lua/conf/plugins/plugins_lsp/markdown.lua
@@ -1,56 +1,74 @@
 return {
 	{
-		"instant-markdown/vim-instant-markdown",
+		"iamcco/markdown-preview.nvim", -- markdown preview plugin for (neo)vim
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
 		ft = { "markdown" },
-		build = "yarn install",
 		config = function()
-			vim.g.instant_markdown_autostart = 0
+			vim.cmd([[
+				let g:mkdp_auto_start = 0
+				let g:mkdp_auto_close = 0
+				let g:mkdp_refresh_slow = 0
+				let g:mkdp_command_for_global = 0
+				let g:mkdp_open_to_the_world = 0
+				" let g:mkdp_open_ip = ''
+				" let g:mkdp_browser = ''
+				let g:mkdp_echo_preview_url = 0
+				" let g:mkdp_browserfunc = ''
+				let g:mkdp_preview_options = {
+						\ 'mkit': {},
+						\ 'katex': {},
+						\ 'uml': {},
+						\ 'maid': {},
+						\ 'disable_sync_scroll': 0,
+						\ 'sync_scroll_type': 'middle',
+						\ 'hide_yaml_meta': 1,
+						\ 'sequence_diagrams': {},
+						\ 'flowchart_diagrams': {},
+						\ 'content_editable': v:false,
+						\ 'disable_filename': 0
+						\ }
+				let g:mkdp_markdown_css = ''
+				let g:mkdp_highlight_css = ''
+				let g:mkdp_port = ''
+				let g:mkdp_page_title = '「${name}」'
+				" let g:mkdp_filetypes = ['markdown']
+				set updatetime=100
+			]])
 		end,
 	},
+	{
+		"dhruvasagar/vim-table-mode", -- VIM Table Mode for instant table creation.
+		cmd = { "TableModeToggle" },
+		ft = { "text", "markdown" },
+		config = function()
+			vim.cmd([[
+				noremap <LEADER>tm :TableModeToggle<CR>
+				"let g:table_mode_disable_mappings = 1
+				let g:table_mode_cell_text_object_i_map = 'k<Bar>'
+			]])
+		end,
+	},
+	{
+		"mzlogin/vim-markdown-toc", -- A vim 7.4+ plugin to generate table of contents for Markdown files.
+		ft = { "gitignore", "markdown" },
+		config = function()
+			-- vim.g.vmt_auto_update_on_save = 0
+			-- vim.g.vmt_dont_insert_fence = 1
+			vim.g.vmt_cycle_list_item_markers = 1
+			vim.g.vmt_fence_text = "TOC"
+			vim.g.vmt_fence_closing_text = "/TOC"
+		end,
+	},
+	-- {
+	-- 	"instant-markdown/vim-instant-markdown", -- Instant Markdown previews from Vim
+	-- 	ft = { "markdown" },
+	-- 	build = "yarn install",
+	-- 	config = function()
+	-- 		vim.g.instant_markdown_autostart = 0
+	-- 	end,
+	-- },
 }
---[[
-{
-	'iamcco/markdown-preview.nvim', 
-	{ 'do': 'cd app && yarn install'  }
-		let g:mkdp_auto_start = 0
-		let g:mkdp_auto_close = 0
-		let g:mkdp_refresh_slow = 0
-		let g:mkdp_command_for_global = 0
-		let g:mkdp_open_to_the_world = 0
-		" let g:mkdp_open_ip = ''
-		" let g:mkdp_browser = ''
-		let g:mkdp_echo_preview_url = 0
-		" let g:mkdp_browserfunc = ''
-		let g:mkdp_preview_options = {
-				\ 'mkit': {},
-				\ 'katex': {},
-				\ 'uml': {},
-				\ 'maid': {},
-				\ 'disable_sync_scroll': 0,
-				\ 'sync_scroll_type': 'middle',
-				\ 'hide_yaml_meta': 1,
-				\ 'sequence_diagrams': {},
-				\ 'flowchart_diagrams': {},
-				\ 'content_editable': v:false,
-				\ 'disable_filename': 0
-				\ }
-		let g:mkdp_markdown_css = ''
-		let g:mkdp_highlight_css = ''
-		let g:mkdp_port = ''
-		let g:mkdp_page_title = '「${name}」'
-		let g:mkdp_filetypes = ['markdown']
-		set updatetime=100
-}
-
-Plug 'dhruvasagar/vim-table-mode', { 'on': 'TableModeToggle', 'for': ['text', 'markdown', 'vim-plug'] }  " VIM Table Mode for instant table creation.
-	noremap <LEADER>tm :TableModeToggle<CR>
-	"let g:table_mode_disable_mappings = 1
-	let g:table_mode_cell_text_object_i_map = 'k<Bar>'
-
-Plug 'mzlogin/vim-markdown-toc', { 'for': ['gitignore', 'markdown', 'vim-plug'] }                        " A vim 7.4+ plugin to generate table of contents for Markdown files.
-	"let g:vmt_auto_update_on_save = 0
-	"let g:vmt_dont_insert_fence = 1
-	let g:vmt_cycle_list_item_markers = 1
-	let g:vmt_fence_text = 'TOC'
-	let g:vmt_fence_closing_text = '/TOC'
-]]--
