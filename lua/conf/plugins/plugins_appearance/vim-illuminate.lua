b8c753687739695611ef89fc54ed53fdf92ae51b
Kuring <kuring@MdeMacBook-Air.local>
2023-11-06 12:15:55 +0800

    vim-illuminate add keymap

--- lua/conf/plugins/plugins_appearance/vim-illuminate.lua
+++ lua/conf/plugins/plugins_appearance/vim-illuminate.lua
@@ -1,14 +1,25 @@
-return
-{ -- automatically highlighting other uses of the current word under the cursor.
-	"RRethy/vim-illuminate",
+return {
+	"RRethy/vim-illuminate", -- automatically highlighting other uses of the current word under the cursor.
 	config = function()
-		require('illuminate').configure({
+		require("illuminate").configure({
 			providers = {
-				-- 'lsp',
-				-- 'treesitter',
-				'regex',
+				"lsp",
+				-- "treesitter",
+				"regex",
 			},
 		})
 		vim.cmd("hi IlluminatedWordText guibg=#393E4D gui=none")
-	end
+		vim.api.nvim_set_keymap("n", "=", "", {
+			noremap = true,
+			callback = function()
+				require("illuminate").goto_next_reference(wrap)
+			end,
+		})
+		vim.api.nvim_set_keymap("n", "-", "", {
+			noremap = true,
+			callback = function()
+				require("illuminate").goto_prev_reference(wrap)
+			end,
+		})
+	end,
 }
