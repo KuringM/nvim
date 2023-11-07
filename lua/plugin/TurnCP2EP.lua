a7875253ef4749d9ac8e7ea051a55e0e5f14431e
Kuring <kuring@MdeMacBook-Air.local>
2023-11-07 23:00:49 +0800

    update

--- lua/plugin/TurnCP2EP.lua
+++ lua/plugin/TurnCP2EP.lua
@@ -1,101 +1,101 @@
 -- Chinese Punctuation turn to English Punctuation
 local CheckChineseMark = function()
-	if vim.ft.search("。") then
+	if vim.fn.search("。") then
 		vim.cmd([[
 			let s:line=search('。')
 			execute s:line . "s/。/\. /g"
 		]])
-	elseif vim.ft.search("，") then
+	elseif vim.fn.search("，") then
 		vim.cmd([[
 			let s:line=search('，')
 			execute s:line . "s/，/, /g"
 		]])
-	elseif vim.ft.search("；") then
+	elseif vim.fn.search("；") then
 		vim.cmd([[
 			let s:line=search('；')
 			execute s:line . "s/；/;/g"
 		]])
-	elseif vim.ft.search("？") then
+	elseif vim.fn.search("？") then
 		vim.cmd([[
 			let s:line=search('？')
 			execute s:line . "s/？/?/g"
 		]])
-	elseif vim.ft.search("：") then
+	elseif vim.fn.search("：") then
 		vim.cmd([[
 			let s:line=search('：')
 			execute s:line . "s/：/\:/g"
 		]])
-	elseif vim.ft.search("‘") then
+	elseif vim.fn.search("‘") then
 		vim.cmd([[
 			let s:line=search('‘')
 			execute s:line . "s/‘/\'/g"
 		]])
-	elseif vim.ft.search("’") then
+	elseif vim.fn.search("’") then
 		vim.cmd([[
 			let s:line=search('’')
 			execute s:line . "s/’/\'/g"
 		]])
-	elseif vim.ft.search("”") then
+	elseif vim.fn.search("”") then
 		vim.cmd([[
 			let s:line=search('”')
 			execute s:line . "s/”/\"/g"
 		]])
-	elseif vim.ft.search("“") then
+	elseif vim.fn.search("“") then
 		vim.cmd([[
 			let s:line=search('“')
 			execute s:line . "s/“/\"/g"
 		]])
-	elseif vim.ft.search("《") then
+	elseif vim.fn.search("《") then
 		vim.cmd([[
 			let s:line=search('《')
 			execute s:line . "s/《/\</g"
 		]])
-	elseif vim.ft.search("》") then
+	elseif vim.fn.search("》") then
 		vim.cmd([[
 			let s:linie=search('》')
 			execute s:line . "s/》/\>/g"
 		]])
-	elseif vim.ft.search("——") then
+	elseif vim.fn.search("——") then
 		vim.cmd([[
 			let s:line=search('——')
 		execute s:line . "s/——/-/g"
 		]])
-	elseif vim.ft.search("）") then
+	elseif vim.fn.search("）") then
 		vim.cmd([[
 			let s:line=search('）')
 			execute s:line . "s/）/\)/g"
 		]])
-	elseif vim.ft.search("（") then
+	elseif vim.fn.search("（") then
 		vim.cmd([[
 			let s:line=search('（')
 			execute s:line . "s/（/\(/g"
 		]])
-	elseif vim.ft.search("……") then
+	elseif vim.fn.search("……") then
 		vim.cmd([[
 			let s:line=search('……')
 			execute s:line . "s/……/^/g"
 		]])
-	elseif vim.ft.search("¥") then
+	elseif vim.fn.search("¥") then
 		vim.cmd([[
 			let s:line=search('¥')
 			execute s:line . "s/¥/$/g"
 		]])
-	elseif vim.ft.search("！") then
+	elseif vim.fn.search("！") then
 		vim.cmd([[
 			let s:line=search('！')
 			execute s:line . "s/！/!/g"
 		]])
-	elseif vim.ft.search("·") then
+	elseif vim.fn.search("·") then
 		vim.cmd([[
 			let s:line=search('·')
 			execute s:line . "s/·/`/g"
 		]])
-	elseif vim.ft.search("【") then
+	elseif vim.fn.search("【") then
 		vim.cmd([[
 			let s:line=search('【')
 			execute s:line . "s/【/[/g"
 		]])
-	elseif vim.ft.search("】") then
+	elseif vim.fn.search("】") then
 		vim.cmd([[
 			let s:line=search('】')
 			execute s:line . "s/】/]/g"
@@ -103,5 +103,5 @@ local CheckChineseMark = function()
 	end
 end
 
-vim.keymap.set("n", "<C-s>" ,":call CheckChineseMark()<CR>", { noremap = true})
+vim.keymap.set("n", "<C-s>" ,CheckChineseMark, { noremap = true})
 vim.keymap.set("i", "<C-s>" ,"<ESC>:call CheckChineseMark()<CR>", { noremap = true})
