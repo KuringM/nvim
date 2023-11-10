25b0fa385b9955c5eaaa3cd159e315fee3f3ba89
Kuring <kuring@MdeMacBook-Air.local>
2023-11-10 17:02:04 +0800

    add nvim-ts-rainbow2

--- lua/conf/plugins/plugins_lsp/nvim-treesitter.lua
+++ lua/conf/plugins/plugins_lsp/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 return {
-	"nvim-treesitter/playground",  -- Treesitter playground integrated into Neovim
-	'nvim-treesitter/nvim-treesitter-refactor', -- Refactor module for nvim-treesitter
+	"nvim-treesitter/playground", -- Treesitter playground integrated into Neovim
+	"nvim-treesitter/nvim-treesitter-refactor", -- Refactor module for nvim-treesitter
 	{
-		"nvim-treesitter/nvim-treesitter",  -- Nvim Treesitter configurations and abstraction layer
+		"nvim-treesitter/nvim-treesitter", -- Nvim Treesitter configurations and abstraction layer
 		lazy = false,
 		priority = 1000,
 		build = ":TSUpdate",
@@ -32,42 +32,58 @@ return {
 					disable = {}, -- list of language that will be disabled
 				},
 				indent = {
-					enable = false
+					enable = false,
 				},
 				incremental_selection = {
 					enable = true,
 					keymaps = {
-						init_selection    = "<c-n>",
-						node_incremental  = "<c-n>",
-						node_decremental  = "<c-h>",
+						init_selection = "<c-n>",
+						node_incremental = "<c-n>",
+						node_decremental = "<c-h>",
 						scope_incremental = "<c-l>",
 					},
-				}
+				},
 			})
-			vim.keymap.set('n', '<f10>', ':TSHighlightCapturesUnderCursor<CR>', {noremap =  true})
-		end
+			vim.keymap.set("n", "<f10>", ":TSHighlightCapturesUnderCursor<CR>", { noremap = true })
+		end,
 	},
 	{
-		"nvim-treesitter/nvim-treesitter-context",  -- Show code context
+		"nvim-treesitter/nvim-treesitter-context", -- Show code context
 		config = function()
-			local tscontext = require('treesitter-context')
-			tscontext.setup {
+			local tscontext = require("treesitter-context")
+			tscontext.setup({
 				enable = true,
-				max_lines = 0,        -- How many lines the window should span. Values <= 0 mean no limit
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit
 				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 				line_numbers = true,
 				multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-				trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = 'cursor',      -- Line used to calculate context. Choices: 'cursor', 'topline'
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
 				-- Separator between context and content. Should be a single character string, like '-'.
 				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 				separator = nil,
 				zindex = 20, -- The Z-index of the context window
 				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-			}
+			})
 			vim.keymap.set("n", "[c", function()
 				tscontext.go_to_context()
 			end, { silent = true })
-		end
+		end,
+	},
+	{
+		"HiPhish/nvim-ts-rainbow2",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				rainbow = {
+					enable = true,
+					-- list of languages you want to disable the plugin for
+					disable = { "jsx", "cpp" },
+					-- Which query to use for finding delimiters
+					query = "rainbow-parens",
+					-- Highlight the entire buffer all at once
+					strategy = require("ts-rainbow").strategy.global,
+				},
+			})
+		end,
 	},
 }
