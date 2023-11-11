dee7a1e5df61459cbe569a7e640d23eae9fe4918
Kuring <kuring@MdeMacBook-Air.local>
2023-11-11 15:04:53 +0800

    update gitsigns_nvim format

--- lua/conf/plugins/plugins_appearance/gitsigns_nvim.lua
+++ lua/conf/plugins/plugins_appearance/gitsigns_nvim.lua
@@ -1,14 +1,44 @@
 return {
 	"lewis6991/gitsigns.nvim", -- Git integration for buffers
 	config = function()
-		require('gitsigns').setup({
+		require("gitsigns").setup({
 			signs = {
-				add          = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-				change       = { hl = 'GitSignsChange', text = '░', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-				delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-				topdelete    = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-				changedelete = { hl = 'GitSignsChange', text = '▒', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-				untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+				add = {
+					hl = "GitSignsAdd",
+					text = "▎",
+					numhl = "GitSignsAddNr",
+					linehl = "GitSignsAddLn",
+				},
+				change = {
+					hl = "GitSignsChange",
+					text = "░",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn",
+				},
+				delete = {
+					hl = "GitSignsDelete",
+					text = "_",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn",
+				},
+				topdelete = {
+					hl = "GitSignsDelete",
+					text = "▔",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn",
+				},
+				changedelete = {
+					hl = "GitSignsChange",
+					text = "▒",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn",
+				},
+				untracked = {
+					hl = "GitSignsAdd",
+					text = "┆",
+					numhl = "GitSignsAddNr",
+					linehl = "GitSignsAddLn",
+				},
 			},
 		})
 		vim.keymap.set("n", "<leader>g-", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
