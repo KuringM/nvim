0403ab9ec637351e4f42d8475fa4f9c10f869d3a
Kuring <kuring@MdeMacBook-Air.local>
2023-12-21 21:19:08 +0800

    update

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require('conf/options')
 require('conf.keymaps')
 require('conf.lazynvim')
-require("plugin.compile_run")
+require("scripts.compile_run")
 -- require("plugin.TurnCP2EP")
 -- require("plugin.vertical_cursor_movement")
 
 -- local swap_ternary = require("plugin.swap_ternary")
 -- vim.keymap.set("n", "<leader>st", swap_ternary.swap_ternary, { noremap = true })
 --
-vim.cmd([[:source $HOME/.config/nvim/lua/plugin/TurnCP2EP.vim]])
+vim.cmd([[:source $HOME/.config/nvim/lua/scripts/TurnCP2EP.vim]])
