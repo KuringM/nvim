fcb6fca2e900914a800ca78ef81cd3f4be04f648
Kuring <kuring@MdeMacBook-Air.local>
2023-12-21 21:37:14 +0800

    update

--- lua/conf/plugins/editor.lua
+++ lua/conf/plugins/editor.lua
@@ -13,6 +13,7 @@ return {
 	require(a .. "tcomment_vim"),       -- An extensible & universal comment vim-plugin that also handles embedded filetypes. in <space>cn to comment a line.
 	require(a .. "vim-visual-multi"),   -- Multiple cursors plugin for vim/neovim
 	require(a .. "undotree"),           -- The undo history visualizer for VIM
+	require(a .. "copilot_vim"),        -- Neovim plugin for GitHub Copilot
 
 	-- Search, Jump, Substitute, Yank, Object...
 	require(a .. "flash_nvim"),         -- Navigate your code with search labels, enhanced character motions and Treesitter integration
@@ -21,16 +22,18 @@ return {
 	require(a .. "nvim-surround"),      -- Add/change/delete surrounding delimiter pairs with ease. Written with ❤️ in Lua.
 	require(a .. "yanky_nvim"),         -- Improved Yank and Put functionalities for Neovim
 	require(a .. "fzf-lua"),            -- Improved fzf.vim written in lua
-	{ "theniceboy/antovim", lazy = false }, -- Super-simple vim plugin for cycling through antonyms/words related to word under cursor
+	require(a .. "telescope_nvim"),     -- Find, Filter, Preview, Pick. All lua, all the time.
+	{ "theniceboy/antovim", lazy = false }, -- Super-simple vim plugin for cycling through antonyms/words related to word under cursor. "gs"
 	{ "gcmt/wildfire.vim", lazy = false }, -- Smart selection of the closest text object
 
 	-- Keyboard Enhance
 	require(a .. "smartim"),            -- A Vim plugin to make it stand well with input methods (Mac only)
 	-- { "lilydjwg/fcitx.vim" },         -- keep and restore fcitx state when leaving/re-entering insert mode
 
-	-- TODO
+	-- Shell Enhance
 	{ "sjl/clam.vim" },                  -- A lightweight Vim plugin for working with shell commands.
-	require(a .. "copilot_vim"),        -- Neovim plugin for GitHub Copilot
+
+	-- TODO
 	require(a .. "vim-rooter"),         -- Changes Vim working directory to project root.
 	-- require(a .. "vim-smoothie"),
 	-- require(a .. "nvim-ufo"),        -- Not UFO in the sky, but an ultra fold in Neovim.
@@ -39,6 +42,5 @@ return {
 	-- require(a .. "LuaSnip"),         -- Snippet Engine for Neovim written in Lua.
 	-- require(a .. "leap_nvim"),       -- Neovim"s answer to the mouse 🦘
 	-- require(a .. "substitute_nvim"), -- Neovim plugin introducing a new operators motions to quickly replace and exchange text.
-	-- require(a .. "telescope_nvim"),  -- Find, Filter, Preview, Pick. All lua, all the time.
 	-- require(a .. "commander_nvim"),
 }
