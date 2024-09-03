1924bd783e0f5d4cad625d6e0861748eed52e0b6
Kuring <kuring@MdeMacBook-Air.local>
2024-09-03 23:04:42 +0800

    update nvim-deus config

--- lua/conf/plugins/plugins_appearance/nvim-deus.lua
+++ lua/conf/plugins/plugins_appearance/nvim-deus.lua
@@ -1,11 +1,11 @@
 return {
-	--"theniceboy/nvim-deus",
-	"Avimitin/neovim-deus", -- The color scheme for neovim
+	"theniceboy/nvim-deus",
+	-- "Avimitin/neovim-deus", -- The color scheme for neovim
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.o.termguicolors = true
-		vim.o.t_Co = 256
+		-- vim.o.t_Co = 256
 		vim.cmd([[
 		let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 		let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
