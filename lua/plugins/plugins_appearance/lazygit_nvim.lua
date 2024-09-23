3a45a3f848e5474df6a0178f1ecb6f844f0ec353
Kuring <361118339@qq.com>
2024-09-23 22:43:49 +0800

    update lazygit.nvim conf

--- lua/plugins/plugins_appearance/lazygit_nvim.lua
+++ lua/plugins/plugins_appearance/lazygit_nvim.lua
@@ -1,10 +1,12 @@
 return {
 	"kdheepak/lazygit.nvim", -- Plugin for calling lazygit from within neovim.
-	keys = { "<c-g>" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<c-g>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	},
 	config = function()
 		vim.g.lazygit_floating_window_scaling_factor = 1.0
 		vim.g.lazygit_floating_window_winblend = 0
 		vim.g.lazygit_use_neovim_remote = true
-		vim.keymap.set("n", "<c-g>", ":LazyGit<CR>", { noremap = true, silent = true })
 	end,
 }
