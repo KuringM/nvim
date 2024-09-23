035fdb842f958fc9924583811ff00b10f71b17c2
Kuring <361118339@qq.com>
2024-09-24 00:23:28 +0800

    update wilder.nvim conf

--- lua/plugins/plugins_appearance/wilder_nvim.lua
+++ lua/plugins/plugins_appearance/wilder_nvim.lua
@@ -3,6 +3,9 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 		"romgrk/fzy-lua-native",
+		"nixprime/cpsm",
+		"roxma/nvim-yarp",
+		"roxma/vim-hug-neovim-rpc",
 	},
 	build = {
 		"luarocks install pcre2",
@@ -10,48 +13,38 @@ return {
 	config = function()
 		local wilder = require("wilder")
 		wilder.setup({
-			modes = { ":" },
+			modes = { ":", "/" },
 			next_key = "<Tab>",
 			previous_key = "<S-Tab>",
 		})
 
-		-- Command Palette
-		-- wilder.set_option(
-		-- 	"renderer",
-		-- 	wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
-		-- 		highlights = {
-		-- 			border = "Normal", -- highlight to use for the border
-		-- 		},
-		-- 		left = { " ", wilder.popupmenu_devicons() },
-		-- 		right = { " ", wilder.popupmenu_scrollbar() },
-		-- 		-- 'single', 'double', 'rounded' or 'solid'
-		-- 		-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
-		-- 		border = "rounded",
-		-- 		max_height = "50%", -- max height of the palette
-		-- 		min_height = 0, -- set to the same as 'max_height' for a fixed height window
-		-- 		prompt_position = "bottom", -- 'top' or 'bottom' to set the location of the prompt
-		-- 		reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
-		-- 	}))
-		-- )
-
 		-- Pipeline
 		wilder.set_option("pipeline", {
 			wilder.branch(
+				wilder.python_file_finder_pipeline({
+					file_command = { "rg", "--files" },
+					dir_command = { "fd", "-tf" },
+					filters = { "fuzzy_filter", "difflib_sorter" },
+				}),
 				wilder.cmdline_pipeline({
-					language = "vim",
+					language = "python",
 					fuzzy = 1,
 				}),
-				wilder.search_pipeline()
+				-- wilder.search_pipeline()
+				wilder.python_search_pipeline({
+					pattern = wilder.python_fuzzy_pattern(),
+					sorter = wilder.python_difflib_sorter(),
+					engine = "re",
+				})
 			),
 		})
 
-		-- Popupmenu renderer
+		-- Popupmenu renderer theme
 		wilder.set_option(
 			"renderer",
-			wilder.popupmenu_renderer({
-				-- highlighter = wilder.basic_highlighter(),
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
 				highlighter = {
-					-- highlighter applies highlighting to the candidates
+					wilder.basic_highlighter(),
 					wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
 					wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found at https://github.com/romgrk/fzy-lua-native
 				},
@@ -61,8 +54,28 @@ return {
 						"Pmenu",
 						{ { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }
 					),
+					border = "Normal",
 				},
-			})
+				left = { " ", wilder.popupmenu_devicons() },
+				right = { " ", wilder.popupmenu_scrollbar() },
+				border = "rounded",
+			}))
 		)
+
+		--  popupmenu palette theme
+		-- wilder.set_option('renderer', wilder.popupmenu_renderer(
+		-- 	wilder.popupmenu_palette_theme({
+		-- 		highlights = {
+		-- 			border = 'Normal', -- highlight to use for the border
+		-- 		},
+		-- 		left = { ' ', wilder.popupmenu_devicons() },
+		-- 		right = { ' ', wilder.popupmenu_scrollbar() },
+		-- 		border = 'rounded',
+		-- 		max_height = '75%',  -- max height of the palette
+		-- 		min_height = 0,      -- set to the same as 'max_height' for a fixed height window
+		-- 		prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+		-- 		reverse = 0,         -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+		-- 	})
+		-- ))
 	end,
 }
