059a52fd4cd435fcce91c88cd5329440b7ff152c
Kuring <361118339@qq.com>
2024-09-25 15:37:49 +0800

    update bufferline.nvim config

--- lua/plugins/plugins_appearance/bufferline_nvim.lua
+++ lua/plugins/plugins_appearance/bufferline_nvim.lua
@@ -1,11 +1,9 @@
 return {
 	"akinsho/bufferline.nvim", -- A snazzy bufferline for Neovim
-	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	opts = {
 		options = {
 			mode = "tabs",
-			-- diagnostics = "nvim_lsp",
 			diagnostics = "coc",
 			diagnostics_indicator = function(count, level, diagnostics_dict, context)
 				local icon = level:match("error") and " " or " "
@@ -28,18 +26,17 @@ return {
 				delay = 200,
 				reveal = {'close'}
 			},
-			
 		},
 	},
 	config = function ()
 		require('bufferline').setup({
 			highlights = {
 				fill = {
-					bg = vim.g.terminal_color_8,
+					bg = vim.api.nvim_get_hl_by_name("Normal", true).background,
 				},
-				background = {
-					bg = vim.g.terminal_color_8,
-				},
+				-- background = {
+				-- 	bg = vim.g.terminal_color_8,
+				-- },
 			},
 		})
 	end,
