b610afb37288fcd51fd739d5ee5f73daabd6d89c
Kuring <361118339@qq.com>
2024-09-28 22:49:26 +0800

    update markdonw hi

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -6,12 +6,12 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*.md", com
 
 local HGS ={
 	-- H1~H6
-	{ hg = "@text.title.1" , style = { fg = colors.red        , bold = true} } ,
-	{ hg = '@text.title.2' , style = { fg = colors.orange     , bold = true} } ,
-	{ hg = '@text.title.3' , style = { fg = colors.yellow     , bold = true} } ,
-	{ hg = '@text.title.4' , style = { fg = colors.green_dark , bold = true} } ,
-	{ hg = '@text.title.5' , style = { fg = colors.cyan       , bold = true} } ,
-	{ hg = '@text.title.6' , style = { fg = colors.purple     , bold = true} } ,
+	{ hg = "@markup.heading.1" , style = { fg = colors.red        , bold = true} } ,
+	{ hg = '@markup.heading.2' , style = { fg = colors.orange     , bold = true} } ,
+	{ hg = '@markup.heading.3' , style = { fg = colors.yellow     , bold = true} } ,
+	{ hg = '@markup.heading.4' , style = { fg = colors.green_dark , bold = true} } ,
+	{ hg = '@markup.heading.5' , style = { fg = colors.cyan       , bold = true} } ,
+	{ hg = '@markup.heading.6' , style = { fg = colors.purple     , bold = true} } ,
 
 	-- H1-H6's markers
 	{ hg = '@text.title.1.marker' , style = { fg = colors.orange } } ,
@@ -22,11 +22,12 @@ local HGS ={
 	{ hg = '@text.title.6.marker' , style = { fg = colors.orange } } ,
 
 	-- Italic, Bold, BoldItalic, Strike, LaTEX environment, LaTEX function
-	{ hg = '@text.emphasis'    , style = { fg = colors.gray       , italic        = true                 } } ,
-	{ hg = '@text.strong'      , style = { fg = colors.gray_light , bold          = true                 } } ,
-	{ hg = '@text.strike'      , style = { fg = colors.red_dark   , strikethrough = true , italic = true } } ,
-	{ hg = '@text.environment' , style = { fg = colors.gray       , bold          = true                 } } ,
-	{ hg = '@function'         , style = { fg = colors.gray       , italic        = true                 } } ,
+	{ hg = '@markup.emphasis'    , style = { fg = colors.gray       , italic        = true                 } } ,
+	{ hg = '@markup.italic'      , style = { fg = colors.gray       , italic        = true                 } } ,
+	{ hg = '@markup.strong'      , style = { fg = colors.gray_light , bold          = true                 } } ,
+	{ hg = '@markup.strike'      , style = { fg = colors.red_dark   , strikethrough = true , italic = true } } ,
+	{ hg = '@markup.environment' , style = { fg = colors.gray       , bold          = true                 } } ,
+	{ hg = '@function'           , style = { fg = colors.gray       , italic        = true                 } } ,
 }
 
 for _,HG in pairs(HGS) do
