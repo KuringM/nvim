5457cca2201729f5c2272a4f2ae13f69eebbfe74
Kuring <361118339@qq.com>
2024-10-26 11:53:40 +0800

    highlighting a selection on yank

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,3 +6,13 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { "*.json" },
 	command = "set filetype=jsonc",
 })
+
+-- highlighting a selection on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
