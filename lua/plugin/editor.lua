ec0db519baf4be9d94b663fb0163697a574f2e3e
Kuring <361118339@qq.com>
2024-10-26 12:09:14 +0800

    remove plugin: move.nvim & wildfire.vim

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -6,7 +6,6 @@ return {
 	{ "sjl/clam.vim" },                     -- A lightweight Vim plugin for working with shell commands.
 	{ "AmaiSaeta/capture.vim" },            -- Capture ex-command output to new buffer.
 	{ "theniceboy/antovim", lazy = false }, -- Super-simple vim plugin for cycling through antonyms/words related to word under cursor. "gs"
-	{ "gcmt/wildfire.vim", lazy = false },  -- Smart selection of the closest text object
 	-- { "lilydjwg/fcitx.vim" },            -- keep and restore fcitx state when leaving/re-entering insert mode
 	require(a .. "smartim"),                -- A Vim plugin to make it stand well with input methods (Mac only)
 	require(a .. "nvim-autopairs"),         -- autopairs for neovim written by lua
@@ -15,7 +14,6 @@ return {
 	require(a .. "tabular"),                -- vim script for text filtering and alignment
 	require(a .. "conform_nvim"),           -- Lightweight yet powerful formatter plugin for Neovim
 	require(a .. "Comment_nvim"),           -- 🧠 💪 // Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more	
-	require(a .. "move_nvim"),              -- Gain the power to move lines and blocks and auto-indent them!
 	require(a .. "substitute_nvim"),        -- Neovim plugin introducing a new operators motions to quickly replace and exchange text.
 	require(a .. "nvim-neoclip"),           -- Clipboard manager neovim plugin with telescope integration
 	require(a .. "vim-visual-multi"),       -- Multiple cursors plugin for vim/neovim
