c8abd780cc193425242da83906d3d83b66db48a7
Kuring <361118339@qq.com>
2025-04-23 22:43:51 +0800

    update

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -3,9 +3,8 @@ local a = "plugin/lsp/"
 return {
 	{ "fladson/vim-kitty" }, -- Vim syntax highlighting for Kitty terminal config files
 	require(a .. "markdown"),
+	require(a .. "vimtex"),
 	require(a .. "nvim-treesitter"),
 	require(a .. "nvim-dap"),
 	require(a .. "coc_nvim"),
-	-- require(a .. "coq_nvim"),
-	-- require(a .. "nvim-cmp"),
 }
