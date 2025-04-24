7d951ff6992bae1ac9657fc21b491f6ea4af157b
Kuring <361118339@qq.com>
2025-04-25 00:53:14 +0800

    update

--- lua/plugin/rice.lua
+++ lua/plugin/rice.lua
@@ -11,14 +11,14 @@ return {
 	require(a .. "lualine_nvim"),         -- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
 	require(a .. "bufferline_nvim"),      -- A snazzy bufferline for Neovim
 	require(a .. "nvim-scrollbar"),       -- Extensible Neovim Scrollbar
-	require(a .. "statuscol_nvim"),       -- Status column plugin that provides a configurable 'statuscolumn' and click handlers.
+	-- require(a .. "statuscol_nvim"),       -- Status column plugin that provides a configurable 'statuscolumn' and click handlers.
+	-- require(a .. "nvim-ufo"),             -- Not UFO in the sky, but an ultra fold in Neovim.
 	require(a .. "wilder_nvim"),          -- A more adventurous wildmenu
 	require(a .. "hlchunk_nvim"),         -- This is the lua implementation of nvim-hlchunk, you can use this neovim plugin to highlight your indent line and the current chunk (context) your cursor stayed
 	require(a .. "nvim-colorizer"),       -- A high-performance color highlighter for Neovim which has no external dependencies! Written in performant Luajit.
 	require(a .. "colorful-winsep_nvim"), -- Make your nvim window separators colorful
 	require(a .. "which-key"),            -- 💥 Create key bindings that stick. WhichKey helps you remember your Neovim keymaps, by showing available keybindings in a popup as you type.
 	require(a .. "dropbar_nvim"),         -- IDE-like breadcrumbs, out of the box
-	require(a .. "nvim-ufo"),             -- Not UFO in the sky, but an ultra fold in Neovim.
 	require(a .. "nvim-notify"),          -- A fancy, configurable, notification manager for NeoVim
 	require(a .. "undotree"),             -- The undo history visualizer for VIM
 	require(a .. "Tabmerge"),             -- Merge a tab"s windows with the current tab
