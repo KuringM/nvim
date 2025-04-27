4a134f1c23d0a09f62c318a5e64234d8e84206bd
Kuring <361118339@qq.com>
2025-04-27 16:51:31 +0800

    update

--- lua/plugin/rice/telescope_nvim.lua
+++ lua/plugin/rice/telescope_nvim.lua
@@ -79,14 +79,6 @@ return {
 					"--trim",
 				},
 			},
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
 		})
 
 		ts.load_extension("fzf")
