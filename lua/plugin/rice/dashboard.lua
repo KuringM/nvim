b17ee8da539dff7375c39b83a6b9fa5c82dfabda
Kuring <361118339@qq.com>
2025-04-27 11:57:11 +0800

    update

--- lua/plugin/rice/dashboard.lua
+++ lua/plugin/rice/dashboard.lua
@@ -1,10 +1,28 @@
+local G = require("G")
+local colors = require("colors")
+
 return {
 	"nvimdev/dashboard-nvim",
 	event = "VimEnter",
 	config = function()
 		require("dashboard").setup({
-			-- config
+			theme = "hyper",
+			-- preview = {
+			-- 	command = "lolcrab",
+			-- 	file_path = "~/.config/nvim/dashboard.txt",
+			-- 	file_width = 69,
+			-- 	file_heigt = 10,
+			-- },
 		})
+		G.hl({
+			{ "DashboardHeader"       , { fg = colors.orange } }     ,
+			{ "DashboardShortCut"     , { fg = colors.orange } }     ,
+			{ "DashboardProjectTitle" , { fg = colors.orange } }     ,
+			{ "DashboardMruTitle"     , { fg = colors.orange } }     ,
+			{ "DashboardFiles"        , { fg = colors.green_dark } } ,
+			{ "DashboardFooter"       , { fg = colors.orange } }     ,
+		})
+		vim.g.dashboard_custom_header = {}
 	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
