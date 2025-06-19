812e5c8155e1e2e5594cf807d8fad7c8d8b8d437
Kuring <361118339@qq.com>
2025-06-19 17:23:08 +0800

    update

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
-require("config.options")
-require("config/keymaps")
-require("config/autocmd")
-require("lazynvim")
-require("script.compile_run")
-require("script.sortABC")
-require("script.TurnCP2EP")
+local modules = {
+	"config.options",
+	"config.keymaps",
+	"config.autocmd",
+	"lazynvim",
+	"script.compile_run",
+	"script.sortABC",
+	"script.TurnCP2EP",
+}
+
+for _, mod in ipairs(modules) do
+	local ok, m = pcall(require, mod)
+	if not ok then
+		vim.notify("加载模块失败: " .. mod .. "\n错误: " .. tostring(m), vim.log.levels.ERROR)
+	end
+end
