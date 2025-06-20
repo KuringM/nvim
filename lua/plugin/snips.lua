5d96beefaf2b746b7b5fa05d3584d701a5ae7687
Kuring <361118339@qq.com>
2025-06-21 00:56:46 +0800

    update

--- lua/plugin/snips.lua
+++ lua/plugin/snips.lua
@@ -7,7 +7,7 @@ return {
 			vim.g.UltiSnipsJumpForwardTrigger = "<c-e>"
 			vim.g.UltiSnipsJumpBackwardTrigger = "<c-u>"
 			vim.g.UltiSnipsJumpOrExpandTrigger = "<c-e>"
-			vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", "UltiSnips/tex" }
+			vim.g.UltiSnipsSnippetDirectories = { "snippets", "snippets/tex" }
 		end,
 	},
 }
