d1ead3c34475dc829105c917c3f5844a693c60ab
Kuring <361118339@qq.com>
2025-08-01 13:21:56 +0800

    update

--- lua/plugin/dashboard.lua
+++ lua/plugin/dashboard.lua
@@ -1,33 +1,55 @@
--- https://github.com//nvimdev/dashboard-nvim
--- vim dashboard
+-- https://github.com/goolord/alpha-nvim
+-- a lua powered greeter like vim-startify / dashboard-nvim
 
-local G = require("G")
-local colors = require("colors")
+local function cfg()
+	local alpha = require("alpha")
+	local dashboard = require("alpha.themes.dashboard")
+	-- Set header
+	dashboard.section.header.val = {
+		"                                                      ",
+		"    ▄▄▄██▀▀▀▒█████ ▓██   ██▓ ▄▄▄▄    ▒█████ ▓██   ██▓ ",
+		"      ▒██  ▒██▒  ██▒▒██  ██▒▓█████▄ ▒██▒  ██▒▒██  ██▒ ",
+		"      ░██  ▒██░  ██▒ ▒██ ██░▒██▒ ▄██▒██░  ██▒ ▒██ ██░ ",
+		"   ▓██▄██▓ ▒██   ██░ ░ ▐██▓░▒██░█▀  ▒██   ██░ ░ ▐██▓░ ",
+		"    ▓███▒  ░ ████▓▒░ ░ ██▒▓░░▓█  ▀█▓░ ████▓▒░ ░ ██▒▓░ ",
+		"    ▒▓▒▒░  ░ ▒░▒░▒░   ██▒▒▒ ░▒▓███▀▒░ ▒░▒░▒░   ██▒▒▒  ",
+		"    ▒ ░▒░    ░ ▒ ▒░ ▓██ ░▒░ ▒░▒   ░   ░ ▒ ▒░ ▓██ ░▒░  ",
+		"    ░ ░ ░  ░ ░ ░ ▒  ▒ ▒ ░░   ░    ░ ░ ░ ░ ▒  ▒ ▒ ░░   ",
+		"    ░   ░      ░ ░  ░ ░      ░          ░ ░  ░ ░      ",
+		"                    ░ ░           ░          ░ ░      ",
+		"                                                      ",
+	}
 
-local function dashboard_cfg()
-	require("dashboard").setup({
-		theme = "hyper",
-		-- preview = {
-		-- 	command = "lolcrab",
-		-- 	file_path = "~/.config/nvim/dashboard.txt",
-		-- 	file_width = 69,
-		-- 	file_height = 10,
-		-- },
-	})
-	G.hl({
-		{ "DashboardHeader", { fg = colors.orange } },
-		{ "DashboardShortCut", { fg = colors.orange } },
-		{ "DashboardProjectTitle", { fg = colors.orange } },
-		{ "DashboardMruTitle", { fg = colors.orange } },
-		{ "DashboardFiles", { fg = colors.green_dark } },
-		{ "DashboardFooter", { fg = colors.orange } },
-	})
-	vim.g.dashboard_custom_header = {}
+	-- Set button
+	dashboard.section.buttons.val = {
+		dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+		dashboard.button("f", "  > Find file", ":Telescope find_files <CR>"),
+		dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+		dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+		dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	}
+
+	-- Set footer
+	local lazy_stats = require("lazy").stats() -- Get Lazy.nvim stats
+	dashboard.section.footer.val = {
+		"If You Don't Take Risks, You Can't Create a Future.",
+		" ",
+		"                                  - Monkey D. Luffy",
+		" ",
+		"             Plugins loaded: " .. lazy_stats.loaded .. " / " .. lazy_stats.count,
+	}
+
+	-- Send config to alpha
+	alpha.setup(dashboard.opts)
+
+	-- Disable folding on alpha buffer
+	vim.cmd([[
+      autocmd FileType alpha setlocal nofoldenable
+    ]])
 end
 
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	config = dashboard_cfg,
+	"goolord/alpha-nvim",
+	lazy = false,
+	config = cfg,
 }
