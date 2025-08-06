0ee394c6730ab12ad59286adbaf6b1ca75653442
Kuring <361118339@qq.com>
2025-08-06 22:41:29 +0800

    update

--- lua/plugin/statusline.lua
+++ lua/plugin/statusline.lua
@@ -1,24 +1,95 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 -- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
 
--- use im-select
+local uv = vim.loop
+local ime_icon = ""
+local timer = nil
+
+-- 异步刷新输入法状态
+local function update_ime_status()
+	uv.spawn("im-select", {
+		args = {},
+	}, function(code, signal)
+		-- 读取输出
+		local stdout = uv.new_pipe(false)
+		local handle
+		handle = uv.spawn("im-select", { stdio = { nil, stdout, nil } }, function()
+			stdout:read_stop()
+			stdout:close()
+			handle:close()
+		end)
+		stdout:read_start(function(err, data)
+			if err then
+				return
+			end
+			if data then
+				local id = data:gsub("%s+", "")
+				if id == "com.apple.keylayout.Colemak" then
+					ime_icon = "󰌌 US"
+				elseif id == "com.apple.inputmethod.SCIM.ITABC" then
+					ime_icon = "拼"
+				else
+					ime_icon = ""
+				end
+			end
+		end)
+	end)
+end
+
+-- 定时刷新(每 2 秒)
+timer = uv.new_timer()
+timer:start(0, 2000, vim.schedule_wrap(update_ime_status))
+
+-- lualine 组件
 local function ime_status()
-  -- 运行 shell 命令获取输入法 ID
-  local handle = io.popen("im-select")
-  if handle == nil then return "" end
-  local result = handle:read("*a")
-  handle:close()
+	return ime_icon
+end
 
-  result = result:gsub("%s+", "")  -- 去掉换行符
-
-  -- 根据输入法 ID 返回图标
-  if result == "com.apple.keylayout.Colemak" then
-    return "󰌌 US"  -- 英文图标
-  elseif result == "com.apple.inputmethod.SCIM.ITABC" then
-    return "拼"      -- 拼音
-  else
-    return ""       -- 未知输入法
-  end
+local function cfg()
+	-- cool function for progress
+	local progress = function()
+		return "%l/%L:%c"
+	end
+	require("lualine").setup({
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			globalstatus = true,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+			},
+		},
+		sections = {
+			lualine_a = { "mode", ime_status },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = {},
+			lualine_y = { "filesize", "encoding", "fileformat", "filetype" },
+			lualine_z = { progress, "progress", "selectioncount" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	})
 end
 
 return {
@@ -27,50 +98,5 @@ return {
 	-- You can optionally lazy-load heirline on UiEnter
 	-- to make sure all required plugins and colorschemes are loaded before setup
 	-- event = "UiEnter",
-	config = function()
-		-- cool function for progress
-		local progress = function()
-			return "%l/%L:%c"
-		end
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = true,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-				},
-			},
-			sections = {
-				lualine_a = { "mode", ime_status },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = {},
-				lualine_y = { "filesize", "encoding", "fileformat", "filetype" },
-				lualine_z = { progress, "progress", "selectioncount" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		})
-	end,
+	config = cfg,
 }
